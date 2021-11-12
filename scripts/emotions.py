ffcddb1354727bb413a5291a8c154d6361b4f411
Yusuke Yamasaki <yuu5364yuu@gmail.com>
2021-11-12 15:48:10 +0900

    Create Service Code(bow)

--- scripts/emotions.py
+++ scripts/emotions.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import rospy
-# import moveit_commander
-# import geometry_msgs
-# import rosnode
-# from tf.transformations import quaternion_from_euler
-# import <サービスのメッセージファイル>
+import rospy
+import moveit_commander
+import geometry_msgs
+import rosnode
+from tf.transformations import quaternion_from_euler
 
-# def main():
-"""
+from std_srvs import SetBool, SetBoolResponce 
+
+def main():
+    """
     この中でいつもの定型文を書く。
     node名：emotions
     arm = moveit_commander.MoveGroupCommander("arm")
@@ -20,35 +21,57 @@
     各サービスのインスタンスを作成
     while not rospy.is_shutdown():
         rospy.spin()   
-"""
+    """
+    rospy.init_node("emotions")
 
-# class Preparation_motion(object):
+    while len([s for s in rosnode.get_node_names() if "rviz" in s]) == 0:
+        rospy.sleep(1.0)
 
-    # def __init__(self):
-"""
-        pass
-"""
+    bow = rospy.Service("bow", SetBool, Emotions_Server.bow_motion)
 
-"""
+    print("Node:emotions Ready")
+    rospy.spin()
+
+class Preparation_motion(object):
+    arm = moveit_commander.MoveGroupCommander("arm")
+    gripper = moveit_commander.MoveGroupCommander("gripper")
+    """
     このクラスでは、init , stand_by , emotions_stand_by をそれぞれ関数として持ち、class Emotions_Serverに提供する
     Service_Serverにはならない
     各関数の最後：return 動作結果
-"""
+    """
+    def init(self):
+        print("init_pose")
+        self.arm.set_named_target("init")
+        self.arm.go()
 
-# class Emotions_Server(object): 
-    # def __init__(self):
-"""
-        class Preparation_motionのインスタンス作成
-"""
+class Emotions_Server(object): 
+    preparation = Preparation_motion
 
-    # def bow_motion(self,<クライアントから送られるデータ名>):
-"""
+    arm = moveit_commander.MoveGroupCommander("arm")
+    gripper = moveit_commander.MoveGroupCommander("gripper")
+
+    def bow_motion(self,data):
+        """
         この関数では、bow をする動作をServiceとして提供する
         動作の速度＆加速度の比率を定義
         動作を行う(test.py参照)
         動作の完了報告を返す
-"""
-
+        """
+        resp = SetBoolResponce()
+        if data.data:
+            try:
+                print("bow")
+                self.arm.set_named_target("bow")
+                self.arm.go()
+                self.preparation.init()
+                resp.message = "Success"
+                resp.success = True
+            except:
+                resp.message = "Failure"
+                resp.success = False
+        return resp
+            
     # def tilt_neck_motion(self,<クライアントから送られるデータ名>):
 """
         この関数では、tile_neck をする動作をServiceとして提供する
@@ -81,7 +104,12 @@
         動作の完了報告を返す
 """
 
-#if __name__=="__main__":
-"""
+if __name__ == '__main__':
+    """
     いつものように try except を組んで、main()を実行
-"""
+    """
+    try:
+        if not rospy.is_shutdown():
+            main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ scripts/test_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-]
+
+import rospy
+import moveit_commander
+import rosnode
+
+from std_srvs.srv import SetBool
+
+def main():
+    rospy.init_node("test_client")
+    arm = moveit_commander.MoveGroupCommander("arm")
+    gripper = moveit_commander.MoveGroupCommander("gripper")
+
+    while len([s for s in rosnode.get_node_names() if "rviz" in s]) == 0:
+        rospy.sleep(1.0)
+    
+    rospy.wait_for_service("bow")
+
+    
+    bow = rospy.ServiceProxy("bow",SetBool)
+    bow(True)
+
+if __name__ == '__main__':
+    try:
+        if not rospy.is_shutdown():
+            main()
+    except rospy.ROSInterruptException:
+        pass
